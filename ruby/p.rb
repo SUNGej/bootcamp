7c005948c8c3137559539252ab9ec81c74aef71f
SUNGej <ulkenfkaus@gmail.com>
2022-10-25 16:32:30 +0900

    ruby-print

---
+++ ruby/p.rb
@@ -0,0 +1,26 @@
+string = "string"
+variable = 14
+array = ["a","bc"]
+map = { a: "a", b: "b" }
+boolean = true
+variable_nil = nil
+
+p(string)
+p(variable)
+p(array)
+p(map)
+p(boolean)
+p(variable_nil)
+p("\n")
+
+p string
+p variable
+p array
+p map
+p boolean
+p variable_nil
+p "\n"
+
+p string, variable, array, map, boolean, variable_nil, "\n"
+
+p(string, variable, array, map, boolean, variable_nil, "\n")
