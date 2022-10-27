d2f3f6477d493f5efde445b6554cd9ec948affba
SUNGej <ulkenfkaus@gmail.com>
2022-10-27 16:09:22 +0900

    ruby-if

---
+++ ruby/if.rb
@@ -0,0 +1,11 @@
+number1 = 5
+number2 = 8
+print "number1 = ", number1, ", number2 = ", number2, "\n"
+
+if number1 > number2
+  puts "number1 is greater than number2"
+elsif number1 < number2
+  puts "number1 is less than number2"
+else
+  puts "number1 is same to number2"
+end
