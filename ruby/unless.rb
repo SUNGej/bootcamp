bff72c35a11b8f6cc8f63c541460cd683f9df31c
SUNGej <ulkenfkaus@gmail.com>
2022-10-27 16:09:44 +0900

    ruby-unless

---
+++ ruby/unless.rb
@@ -0,0 +1,15 @@
+number1 = 5
+number2 = 8
+print "number1 = ", number1, ", number2 = ", number2, "\n"
+
+unless number1 == number2
+  puts "number1 is not same to number2"
+end
+
+unless number1 > number2
+  puts "number1 is less than number2"
+end
+
+unless number1 < number2
+  puts "number is greater than number2"
+end
