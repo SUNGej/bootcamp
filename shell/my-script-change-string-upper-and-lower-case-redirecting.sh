2569b21dd9db4c5438bd976f58dedf1c774f71d8
SUNGej <ulkenfkaus@gmail.com>
2022-10-27 11:17:16 +0900

    add-double-quotation-in-ehco

--- shell/my-script-change-string-upper-and-lower-case-redirecting.sh
+++ shell/my-script-change-string-upper-and-lower-case-redirecting.sh
@@ -10,10 +10,10 @@ if [ -n "$string" ]; then
   string_in_file_lower=${string_in_file,,}
   echo $string_in_file_upper > string_changed_to_upper_case.txt
   echo $string_in_file_lower > string_changed_to_lower_case.txt
-  echo check new directory and files.
+  echo "check new directory and files."
 else
-  echo How to use this file.
-  echo run this file with a string argument.
-  echo a new directory \(output\) will be created.
-  echo in output directory, .txt files of your string, changed to upper case and lower case will be created.
+  echo "How to use this file."
+  echo "run this file with a string argument."
+  echo "a new directory (output) will be created."
+  echo "in output directory, .txt files of your string, changed to upper case and lower case will be created."
 fi
