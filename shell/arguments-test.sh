f8760d7bf243a6697609ef202ab2111394243b1e
SUNGej <ulkenfkaus@gmail.com>
2022-10-27 11:39:23 +0900

    add-double-quotation-in-ehco

--- shell/arguments-test.sh
+++ shell/arguments-test.sh
@@ -3,10 +3,10 @@
 number1=$1
 number2=$2
 
-echo number1 = $number1
-echo number2 = $number2
+echo "number1 = ${number1}"
+echo "number2 = ${number2}"
 
-echo $number1 + $number2 = $(($number1 + $number2))
-echo $number1 - $number2 = $(($number1 - $number2))
-echo $number1 "*" $number2 = $(($number1 * $number2))
-echo $number1 / $number2 = $(($number1 / $number2))
+echo "${number1} + ${number2} = $((${number1} + ${number2}))"
+echo "${number1} - ${number2} = $((${number1} - ${number2}))"
+echo "${number1} * ${number2} = $((${number1} * ${number2}))"
+echo "${number1} / ${number2} = $((${number1} / ${number2}))"
--- shell/pipe.sh
+++ shell/pipe.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/bash
 
 #find .sh files in current directory
+echo "these are files in current directory"
+
 ls
-echo these are files in current directory
+
+echo "---------------------"
+
+echo "these are .sh files in current directory"
 ls | grep ".sh"
-echo these are .sh files in current directory
