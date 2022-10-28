33c80f08d9de2cb22c993c8fa513fde39c62c81f
SUNGej <ulkenfkaus@gmail.com>
2022-10-28 13:03:05 +0900

    amend-double-for-using-arguments.sh-no-use-cd

--- shell/double-for-using-arguments.sh
+++ shell/double-for-using-arguments.sh
@@ -6,11 +6,9 @@ mkdir out
 
 for directory_index in $(seq 1 $number)
 do
-  mkdir dir-$directory_index
-  cd dir-$directory_index
+  mkdir out/dir-$directory_index
   for file_index in $(seq 1 $directory_index)
   do
-    touch file-$file_index
+    touch out/dir-$directory_index/file-$file_index
   done
-cd ../
 done
