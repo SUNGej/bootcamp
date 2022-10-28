c5198ffa2030640b9157f50bee5ede7c3623709e
SUNGej <ulkenfkaus@gmail.com>
2022-10-28 18:06:51 +0900

    ruby-array-loop

--- ruby/array-loop.rb
+++ ruby/array-loop.rb
@@ -1,8 +1,18 @@
 array = [1, 2, 3, 4]
 print "array = ", array, "\n"
 
+
+puts "use block( { } or do ~ end ) for array.method when you don't want make new variable"
+puts "use { } in one command line"
+puts "use do - end in two or more command lines"
+
 puts "array.each"
 array.each { |number| puts number }
+
+puts
+
+puts "after array.each { |number| puts number } variable \"number\" doesn't exists"
+puts "puts number #this command occurs error"
 
 puts "array.delete_if"
 array_delete_if_odd = array.delete_if { |number| number.odd? }
