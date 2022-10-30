ee4bfb73bfde692b49f1e7449be1ce65e660ec33
SUNGej <ulkenfkaus@gmail.com>
2022-10-30 16:30:49 +0900

    ruby-regex-test-phone-number

--- ruby/regexp.rb
+++ ruby/regexp.rb
@@ -36,10 +36,6 @@ puts "\\d : decimal"
 print "/\\d/.match(\"a\") = ", /\d/.match("a"), "\n"
 print "/\\d/.match(\"5\") = ", /\d/.match("5"), "\n"
 
-puts "\\D : character no decimal"
-print "/\\d/.match(\"a\") = ", /\d/.match("a"), "\n"
-print "/\\d/.match(\"5\") = ", /\d/.match("5"), "\n"
-
 puts "\\w : character"
 print "/\\w/.match(\"winter\") = ", /\w/.match("winter"), "\n"
 print "/\\w/.match(\"1234\") = ", /\w/.match("1234"), "\n"
@@ -61,3 +57,15 @@ if /\S/.match("    ")
 else
   puts "\"    \" has space"
 end
+
+my_phone_number = "010-3367-9699"
+phone_number_regex = /[0-9]{3}-[0-9]{4}-[0-9]{4}/
+print "my_phone_number = ", my_phone_number, "\n"
+p "phone_number_regex", phone_number_regex
+puts "=> 000-0000-0000"
+if phone_number_regex.match(my_phone_number)
+  puts "my phone number is correct phone number"
+else
+  puts "my phone number is not correct phone number"
+end
+
